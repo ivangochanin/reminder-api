fbe4968e6a7fdb58c15f1e79209052dc794ca394
ivangochanin <ivangochanin@gmail.com>
2023-01-07 11:07:04 +0100

    create tasks 2DO

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// routes
 app.get('/hello', (req, res) => {
     res.send('Reminder API')
 })
+
+
+// app.get('/api/v1/reminders') - get all reminders
+// app.post('/api/v1/reminders') - create a new reminders
+// app.get('/api/v1/reminders/:id') - get single reminder
+// app.patch('/api/v1/reminders/:id') - update reminder
+// app.delete('/api/v1/reminders/:id') - delete reminder
+
 
   app.listen(port, console.log(`Server i listening on port ${port}...`));
