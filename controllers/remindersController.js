4841745f432558f2ae1f58b18c33fd6d43f2c46a
ivangochanin <ivangochanin@gmail.com>
2023-01-11 16:02:15 +0100

    delete & update reminder

--- controllers/remindersController.js
+++ controllers/remindersController.js
@@ -19,29 +19,63 @@ const createReminderController = async (req, res) => {
 };
 
 const getSingleReminderController = async (req, res) => {
-    try {
+	try {
 		/* const getSingleReminder = await Reminder.findOne({ _id: req.params.id }); */
-        // or in two lines: 
-        const {id:reminderID} = req.params
+		// or in two lines:
+		const { id: reminderID } = req.params;
 		const getSingleReminder = await Reminder.findOne({ _id: reminderID });
 
-        if(!getSingleReminder) {
-            return res.status(404).json({msg: `There is no reminder with id: ${reminderID}`})
-        }
+		if (!getSingleReminder) {
+			return res
+				.status(404)
+				.json({ msg: `There is no reminder with id: ${reminderID}` });
+		}
 		res.status(200).json({ getSingleReminder });
-        
 	} catch (error) {
 		res.status(500).json({ msg: error });
 	}
-	
 };
 
-const updateReminderController = (req, res) => {
-	res.send('update reminder');
+const deleteReminderController = async (req, res) => {
+	try {
+		const { id: reminderID } = req.params;
+		const getSingleReminder = await Reminder.findOneAndDelete({
+			_id: reminderID,
+		});
+
+		if (!getSingleReminder) {
+			return res
+				.status(404)
+				.json({ msg: `There is no reminder with id: ${reminderID}` });
+		}
+		res.status(200).json({ getSingleReminder });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
-const deleteReminderController = (req, res) => {
-	res.send('delete reminder');
+const updateReminderController = async (req, res) => {
+	try {
+		const { id: reminderID } = req.params;
+		const getSingleReminder = await Reminder.findOneAndUpdate(
+			{ _id: reminderID },
+			req.body,
+			{
+				new: true,
+				runValidators: true,
+			}
+		);
+
+		if (!getSingleReminder) {
+			return res
+				.status(404)
+				.json({ msg: `There is no reminder with id: ${reminderID}` });
+		}
+
+		res.status(200).json({ getSingleReminder });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
 module.exports = {
