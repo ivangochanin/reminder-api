4c5d51dda04e18916322eafc4e03bc05e68c8eee
ivangochanin <ivangochanin@gmail.com>
2023-01-21 00:12:10 +0100

    add cors list options

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ const remindersRoutes = require('./routes/remindersRoutes');
 const categoriesRoutes = require('./routes/categoriesRoutes');
 const subCategoriesRoutes = require('./routes/subCategoriesRoutes');
 const connectDB = require('./db/databaseConnect');
+const cors = require('cors')
 require('dotenv').config() // we do not need to store in variable
-
 // middleware - if we don't use this, we won't have data in raq.body
+
+const allowCorsList = [process.env.ADMIN_URL, process.env.FRONTEND_URL]
+
+/* const corsOptions = {
+	origin: allowCorsList,
+	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  } */
+
+ const corsOptionsDelegate = function (req, callback) {
+	let corsOptions;
+	if (allowCorsList.indexOf(req.header('Origin')) !== -1) {
+	  corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+	} else {
+	  corsOptions = { origin: false } // disable CORS for this request
+	}
+	callback(null, corsOptions) // callback expects two parameters: error and options
+  }
+
+app.use(cors(corsOptionsDelegate))
+
 app.use(express.json());
 
 // root route
