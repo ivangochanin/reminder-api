40c4e0d03c6cbc8b54ef2bb3f8da9097e7a79558
ivangochanin <ivangochanin@gmail.com>
2023-01-21 13:22:12 +0100

    change port to 10000 - enable react  ... cors ...

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use('/api/v1/reminders', remindersRoutes);
 app.use('/api/v1/admin/reminders', remindersRoutes);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 const startBackend = async () => {
 	try {
