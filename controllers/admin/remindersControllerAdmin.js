c60eae793bb193af1eb57615aa7f7de484edd69a
ivangochanin <ivangochanin@gmail.com>
2023-01-25 17:17:20 +0100

    delete reminder

--- controllers/admin/remindersControllerAdmin.js
+++ controllers/admin/remindersControllerAdmin.js
@@ -62,21 +62,35 @@ const getSingleReminderController = async (req, res) => {
 };
 
 const deleteReminderController = async (req, res) => {
+	const { id: reminderID } = req.params;
+	const getSingleReminder = await Reminder.findOne({ _id: reminderID });
+	const {subcategory: subcategoryId} =  getSingleReminder
+	
 	try {
-		const { id: reminderID } = req.params;
-		const getSingleReminder = await Reminder.findOneAndDelete({
+		const deleteSingleReminder = await Reminder.findOneAndDelete({
 			_id: reminderID,
 		});
-
-		if (!getSingleReminder) {
+   
+		if (!deleteSingleReminder) {
 			return res
 				.status(404)
 				.json({ msg: `There is no reminder with id: ${reminderID}` });
 		}
-		res.status(200).json({ getSingleReminder });
+		SubCategory.findByIdAndUpdate(subcategoryId,
+			{
+				$pullAll: { reminders: [reminderID] }
+			},
+			(error, subCategory) => {
+				if (error){
+					return res.status(500).json({ msg: error });
+				}
+				return res.status(201).json({ deleteSingleReminder, subCategory });
+			}
+		)
+		
 	} catch (error) {
 		res.status(500).json({ msg: error });
-	}
+	}  
 };
 
 const updateReminderController = async (req, res) => {
